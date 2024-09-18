07fab1f0812c3b5896f4e3b69b7c9578737179e9
shibsankar-stu <dasshibsankar94732@gmail.com>
2024-09-19 02:05:00 +0530

    Guess number app

---
+++ script.js
@@ -0,0 +1,48 @@
+let result = document.getElementById("result")
+let input = document.getElementById("input")
+let btn = document.getElementById("btn")
+let rePlay = document.getElementById("rePlay")
+let preGuess = document.getElementById("preGuess")
+let guessRemaining = document.getElementById("guessRemaining")
+
+preGuess.innerText = 0;
+guessRemaining.innerText = 10;
+btn.addEventListener("click", () => {
+    if (input.value == '' || input.value > 5) {
+        alert("Please Enter any number between 1 and 5")
+    }else{
+        checkValue()
+        if (checkValue()) {
+            result.innerText = "Yohoo🥳"
+            preGuess.innerText++
+            guessRemaining.innerText--;
+            input.value = '';
+        }else{
+            result.innerText = "Opss!🥹";
+            guessRemaining.innerText--;
+            input.value = '';
+        }
+        if(guessRemaining.innerText == 0){
+            result.innerText = `Your Total Score: ${preGuess.innerText}`;
+            btn.remove();
+        }
+    }
+})
+
+function randomNum(){
+    let num = Math.floor(Math.random() * 5)
+    return num
+}
+
+function checkValue(){
+    let inputValue = input.value;
+    let randomValue = randomNum();
+    if(inputValue == randomValue){
+        return true;
+    }else{
+        return false;
+    }
+}
+rePlay.addEventListener("click", () => {
+    window.location.reload();
+})
